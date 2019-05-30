050a6f1d6bed86b2bf11bc876e68354aedb02cbe
Muosvr <lujason2015@gmail.com>
2019-05-30 12:09:16 -0400

    finished

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
 const numbers = [67, 80, 65, 79, 86, 66, 75, 71, 81, 69, 90, 79, 68, 65, 73, 88];
 
-// 1. Convert numbers to Alphabet according to Ascii table using the fromCharCode method
-// Using a for loop:
-// Using the forEach method:
-// Using the map method:
+function run() {
+  // 1. Convert numbers to Alphabet according to Ascii table using the fromCharCode method
+  // Using a for loop:
+  for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i])
+    document.getElementById('forloop').innerHTML += " " + String.fromCharCode(numbers[i])
+  }
 
-// 2. Display numbers below showing only numbers that are greater than 72 and less then 88 (inclusive) using the filter method
-// Answer:
+  // Using the forEach method:
+  numbers.forEach(element => {
+    document.getElementById('foreach').innerHTML += " " + String.fromCharCode(element)
+  })
 
-// 3. Display the product of all numbers using reduce
-// Answer:
+  // Using the map method:
+  const characterArr = numbers.map(element => {
+    return String.fromCharCode(element);
+  })
+  document.getElementById('map').innerHTML += " " + characterArr.join(" ")
+
+
+  // 2. Display numbers below showing only numbers that are greater than 72 and less then 88 (inclusive) using the filter method
+  // Answer:
+  const filteredArr = numbers.filter(element => {
+    return element > 72 && element <= 88
+  })
+  console.log(filteredArr);
+  document.getElementById('filter').innerHTML += " " + filteredArr.join(" ")
+
+  // 3. Display the product of all numbers using reduce
+  // Answer:
+  const product = numbers.reduce((prev, curr) => {
+    return prev * curr
+  })
+
+  document.getElementById('reduce').innerHTML += " " + product
+
+}
+
